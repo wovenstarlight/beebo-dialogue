5f7246f3daa390e434049d9404bcd8ffc39cc1d6
Devishi Ranjan <drconker@gmail.com>
2025-02-15 19:14:34 +0530

    Track form inputs via states

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,19 +1,48 @@
+import { useState } from "react";
 import "../styles/AddForm.css";
 
 function AddForm() {
+	const [speaker, setSpeaker] = useState("");
+	const [dialogue, setDialogue] = useState("");
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		alert(`Trying to create dialogue box with "${speaker}" saying "${dialogue}"`);
+		setSpeaker("");		// clear the value
+		setDialogue("");	// clear the value
+		e.target.reset();	// and remove the invalid markers
+	}
+
 	return (
-		<form id="addform" name="addform">
+		<form id="addform" name="addform" onSubmit={handleSubmit}>
 			<label>
 				<span className="labeltext">Speaker</span>
-				<input id="inputspeaker" name="inputspeaker" type="text" minLength={1} maxLength={50} required />
+				<input
+				id="inputspeaker"
+				name="inputspeaker"
+				type="text"
+				minLength={1}
+				maxLength={50}
+				required
+				value={speaker}
+				onChange={(e) => setSpeaker(e.target.value)}
+				/>
 			</label>
 			{/* Select for the portrait */}
 			<label>
 				<span className="labeltext">Dialogue</span>
-				<textarea id="inputdialogue" name="inputdialogue" type="text" minLength={1} maxLength={110} required />
+				<textarea
+				id="inputdialogue"
+				name="inputdialogue"
+				minLength={1}
+				maxLength={110}
+				required
+				value={dialogue}
+				onChange={(e) => setDialogue(e.target.value)}
+				/>
 			</label>
-			<button id="addbtn">Add</button>
-			<button id="clearbtn">Clear</button>
+			<button id="addbtn" type="submit">Add</button>
+			<button id="clearbtn" type="reset">Clear</button>
 		</form>
 	)
 }
