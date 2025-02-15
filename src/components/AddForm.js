50d6b77528e61efccfcd76ff44037e2386b812b8
Devishi Ranjan <drconker@gmail.com>
2025-02-15 19:32:32 +0530

    Clean form formatting

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -8,37 +8,41 @@ function AddForm() {
 	function handleSubmit(e) {
 		e.preventDefault();
 		alert(`Trying to create dialogue box with "${speaker}" saying "${dialogue}"`);
-		setSpeaker("");		// clear the value
-		setDialogue("");	// clear the value
-		e.target.reset();	// and remove the invalid markers
+		// Clear stored values
+		setSpeaker("");
+		setDialogue("");
+		// And remove the invalid markers for 
+		e.target.reset();
 	}
 
 	return (
 		<form id="addform" name="addform" onSubmit={handleSubmit}>
-			<label>
+			<label id="labelspeaker">
 				<span className="labeltext">Speaker</span>
 				<input
-				id="inputspeaker"
-				name="inputspeaker"
-				type="text"
-				minLength={1}
-				maxLength={50}
-				required
-				value={speaker}
-				onChange={(e) => setSpeaker(e.target.value)}
+					id="inputspeaker"
+					name="inputspeaker"
+					type="text"
+					minLength={1}
+					maxLength={50}
+					required
+					value={speaker}
+					onChange={(e) => setSpeaker(e.target.value)}
 				/>
 			</label>
+
 			{/* Select for the portrait */}
-			<label>
+
+			<label id="labeldialogue">
 				<span className="labeltext">Dialogue</span>
 				<textarea
-				id="inputdialogue"
-				name="inputdialogue"
-				minLength={1}
-				maxLength={110}
-				required
-				value={dialogue}
-				onChange={(e) => setDialogue(e.target.value)}
+					id="inputdialogue"
+					name="inputdialogue"
+					minLength={1}
+					maxLength={110}
+					required
+					value={dialogue}
+					onChange={(e) => setDialogue(e.target.value)}
 				/>
 			</label>
 			<button id="addbtn" type="submit">Add</button>
