8f4684842f23941ec41383c998f68da125ae9f0d
Devishi Ranjan <drconker@gmail.com>
2025-02-15 21:54:22 +0530

    Add/style dialogue deletion button

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,6 +1,10 @@
 import "../styles/TextBox.css";
 
-function TextBox({ id, color, speaker, portrait, dialogue }) {
+function TextBox({ id, color, speaker, portrait, dialogue, setDialogues }) {
+	function deleteBox(e) {
+		setDialogues(dialogues => dialogues.filter(el => el.id !== e.target.parentElement.id));
+	}
+
 	return <article id={id} className={`dialogue ${color ?? "house"}`}>
 		<header className="speaker">
 			<span className="question">?</span>
@@ -10,6 +14,7 @@ function TextBox({ id, color, speaker, portrait, dialogue }) {
 			<img className="portrait" src={`${process.env.PUBLIC_URL}/assets/portraits/`.concat(portrait ?? "default.png")} />
 			<p className="text">{dialogue ?? "Lorem ipsum dolor sit amet."}</p>
 		</div>
+		<button className="delete" onClick={deleteBox}>Delete</button>
 	</article>
 }
 
