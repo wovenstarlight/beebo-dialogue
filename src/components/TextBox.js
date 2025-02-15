36c552531c7f32356823b5eff7646a7ad5729a04
Devishi Ranjan <drconker@gmail.com>
2025-02-16 03:10:26 +0530

    Adjust ? icon pixel sizing and ARIA detection

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -73,7 +73,7 @@ function TextBox({ id, color, speaker, portrait, dialogue, allDialogues, setDial
 				<span className="name">{speaker ?? "Y/N"}</span>
 			</header>
 			<div className="body">
-				<img className="portrait" alt="" src={`${process.env.PUBLIC_URL}/assets/portraits/`.concat(portrait ?? "default.png")} />
+				<img className="portrait" alt="" src={`${process.env.PUBLIC_URL}/assets/portraits/`.concat(portrait ?? "default.png")} aria-hidden="true" />
 				<p className="text">{dialogue ?? "Lorem ipsum dolor sit amet."}</p>
 			</div>
 
