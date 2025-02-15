93e833ffb0d626c5c3b7f39efda44bc6a26a2758
Devishi Ranjan <drconker@gmail.com>
2025-02-15 20:40:39 +0530

    Add data (colors and some sample portraits)

---
+++ src/assets/data/colors.js
@@ -0,0 +1,15 @@
+const allColors = {
+	"Purple (House of Vera/Eugene Coli)": "purple",
+	"Orange (Oliver Beebo)": "orange",
+	"Gray (Ángel Valdivia)": "gray",
+	"Red (Vivi Villalobos)": "red",
+	"Blue (Marigold Margulis)": "blue",
+	"Bluish purple (Nina Coli)": "blueypurple",
+	"Pink (Nadia Margulis)": "pink",
+	"Green (Simon Margulis)": "green",
+	"Purplish blue (Owen)": "navy",
+	"Yellow (Art gallery)": "yellow",
+	"Dim gray (City/Police)": "dimgray",
+}
+
+export default allColors;
---
+++ src/assets/data/portraits.js
@@ -0,0 +1,22 @@
+const allPortraits = {
+	"Ángel (House)": {
+		"Ángel > Sad smile": "angel/house/sadsmile.png",
+		"Ángel > Hehe": "angel/house/hehe.png",
+	},
+	"Ángel (Gray)": {
+		"Ángel > Sad smile": "angel/color/sadsmile.png",
+		"Ángel > Hehe": "angel/color/hehe.png",
+	},
+	"Vivi (House)": {
+		"Vivi > Hehe": "vivi/house/hehe.png",
+	},
+	"Vivi (Red)": {
+		"Vivi > Hehe": "vivi/color/hehe.png",
+	},
+	"Oliver (Orange)": {
+		"Oliver > Happy": "oliver/color/happy.png",
+		"Oliver > Sweet": "oliver/color/sweet.png",
+	}
+}
+
+export default allPortraits;
