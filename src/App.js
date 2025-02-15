57ddf242e1a5b954dcb5c04e1b17a9ebcdf1a3a3
Devishi Ranjan <drconker@gmail.com>
2025-02-15 19:08:33 +0530

    Replace template with basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (<>
+		<header id="sitehead">
+			<h1>Detective Beebo dialogue builder</h1>
+			<p>Based on the game <a href="https://bwobbers.itch.io/detective-beebo-night-at-the-mansion">Detective Beebo: Night at the Mansion</a> by Bwobbers</p>
+			<p>(Inspired by and built off <a href="https://kongkrog.github.io/isat-profile-customizer/beebo/beebo.html">GIF version</a>)</p>
+		</header>
+		<main id="sitebody">
+			<p id="explainer">TODO</p>
+		</main>
+		<footer id="sitefoot">
+			<p>Made by <a href="https://github.com/wovenstarlight">wovenstarlight</a></p>
+			<p>Font credits: <a href="https://www.dafont.com/nokia-cellphone.font">Nokia Cellphone FC</a> and SqueezedPixels by Bwobbers</p>
+		</footer>
+	</>);
 }
 
 export default App;
