62671cbb4920b58eb7980530a4a20777f5a6c8b2
Devishi Ranjan <drconker@gmail.com>
2025-02-15 23:36:25 +0530

    Add edit menu for each dialogue box

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,12 +1,55 @@
+import { useState } from "react";
 import "../styles/TextBox.css";
+import BaseForm from "./BaseForm";
 
 function TextBox({ id, color, speaker, portrait, dialogue, allDialogues, setDialogues, isFirst, isLast }) {
+	// #region Edit this box
+	/** Temporary variables for editing this dialogue box with. */
+	const [tempColor, setTempColor] = useState(color);
+	const [tempSpeaker, setTempSpeaker] = useState(speaker);
+	const [tempPortrait, setTempPortrait] = useState(portrait);
+	const [tempDialogue, setTempDialogue] = useState(dialogue);
+	const [editIsHidden, setEditIsHidden] = useState(true);
+	/** Open the editing menu for this dialogue box. */
+	function editBox(e) {
+		let article = e.target.closest("article")
+		article.setAttribute("hidden", "true");	// using JS directly to avoid re-rendering
+		setEditIsHidden(false);
+	}
+	/** Update this dialogue box with the edited information. */
+	function handleSubmit(e) {
+		e.preventDefault();
+		let [pre, , post] = getNeighbors();
+		setDialogues(pre.concat([{
+			id: id,
+			color: tempColor,
+			speaker: tempSpeaker,
+			portrait: tempPortrait,
+			dialogue: tempDialogue,
+		}], post));
+		setEditIsHidden(true);
+		// using JS directly to avoid re-rendering again after the update from setDialogues
+		e.target.previousElementSibling.removeAttribute("hidden");
+	}
+	function cancelEdit(e) {
+		// Reset the temporary variables to their original values
+		setTempColor(color);
+		setTempSpeaker(speaker);
+		setTempPortrait(portrait);
+		setTempDialogue(dialogue);
+		// And hide this form
+		setEditIsHidden(true);
+		e.target.closest("form").previousElementSibling.removeAttribute("hidden");	// using JS directly to avoid re-rendering
+	}
+	// #endregion
+
 	/** Deletes the current dialogue box. */
 	function deleteBox() {
 		setDialogues(dialogues => dialogues.filter(el => el.id !== id));
 	}
 
-	/** Finds the neighboring dialogue boxes. Helper for `moveUp`/`moveDown`. */
+	// #region Move this box
+	/** Finds the neighboring dialogue boxes. Helper for `moveUp`/`moveDown` and `edit`. */
 	function getNeighbors() {
 		let index = allDialogues.findIndex(el => el.id === id);
 		return [allDialogues.slice(0, index), allDialogues[index], allDialogues.slice(index + 1)];
@@ -21,23 +64,44 @@ function TextBox({ id, color, speaker, portrait, dialogue, allDialogues, setDial
 		let [pre, curr, post] = getNeighbors();
 		setDialogues(pre.concat([post.at(0), curr], post.slice(1)));
 	}
+	//#endregion
 
-	return <article id={id} className={`dialogue ${color ?? "house"}`}>
-		<header className="speaker">
-			<span className="question">?</span>
-			<span className="name">{speaker ?? "Y/N"}</span>
-		</header>
-		<div className="body">
-			<img className="portrait" src={`${process.env.PUBLIC_URL}/assets/portraits/`.concat(portrait ?? "default.png")} />
-			<p className="text">{dialogue ?? "Lorem ipsum dolor sit amet."}</p>
-		</div>
+	return <>
+		<article id={id} className={`dialogue ${color ?? "house"}`}>
+			<header className="speaker">
+				<span className="question">?</span>
+				<span className="name">{speaker ?? "Y/N"}</span>
+			</header>
+			<div className="body">
+				<img className="portrait" alt="" src={`${process.env.PUBLIC_URL}/assets/portraits/`.concat(portrait ?? "default.png")} />
+				<p className="text">{dialogue ?? "Lorem ipsum dolor sit amet."}</p>
+			</div>
 
-		<div className="buttons">
-			<button className="move" onClick={moveUp} disabled={isFirst}>Move up</button>
-			<button className="delete" onClick={deleteBox}>Delete</button>
-			<button className="move" onClick={moveDown} disabled={isLast}>Move down</button>
-		</div>
-	</article>
+			<div className="buttons">
+				<button className="edit" onClick={editBox}>Edit</button>
+				<button className="delete" onClick={deleteBox}>Delete</button>
+				<button className="move" onClick={moveUp} disabled={isFirst}>Move up</button>
+				<button className="move" onClick={moveDown} disabled={isLast}>Move down</button>
+			</div>
+		</article>
+		<BaseForm
+			id={`edit_${id}`}
+			title="Edit"
+			handleSubmit={handleSubmit}
+			hidden={editIsHidden}
+			color={tempColor}
+			setColor={setTempColor}
+			speaker={tempSpeaker}
+			setSpeaker={setTempSpeaker}
+			dialogue={tempDialogue}
+			setDialogue={setTempDialogue}
+			portrait={tempPortrait}
+			setPortrait={setTempPortrait}
+		>
+			<button className="submitbtn" type="submit">Update</button>
+			<button className="resetbtn" type="reset" onClick={cancelEdit}>Cancel</button>
+		</BaseForm>
+	</>;
 }
 
 export default TextBox;
