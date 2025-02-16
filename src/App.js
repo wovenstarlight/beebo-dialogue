b91ea67036388b78a1e26a6209dce9e05df35983
Devishi Ranjan <drconker@gmail.com>
2025-02-17 01:37:14 +0530

    Change "dialogues" to "blocks" in prep for MCQ

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./styles/App.css";
 import { useState } from "react";
-import DialogueUnit from "./components/DialogueUnit";
+import DialogueBlock from "./components/DialogueBlock";
 import AddForm from "./components/AddForm";
 import JSONInput from "./components/JSONInput";
 import domtoimage from 'dom-to-image';
+import MultipleChoice from "./components/MultipleChoice";
 
 function App() {
-	const [dialogues, setDialogues] = useState([]);
+	const [blocks, setBlocks] = useState([]);
 	
 	/** Deletes all dialogue boxes currently being displayed. */
 	function clearAll() {
 		if (window.confirm("Really delete all currently-generated dialogue boxes?\n\nThis is irreversible!"))
-			setDialogues([]);
+			setBlocks([]);
 	}
 
 	/** Saves all currently-displayed dialogue boxes as an image. */
@@ -48,27 +49,33 @@ function App() {
 			<p>Based on the game <a href="https://bwobbers.itch.io/detective-beebo-night-at-the-mansion">Detective Beebo: Night at the Mansion</a> by Bwobbers</p>
 			<p>A static image version of <a href="https://kongkrog.github.io/isat-profile-customizer/beebo/beebo.html">kongkrog's dialogue GIF maker</a></p>
 		</header>
+
 		<main id="sitebody">
-			{dialogues.length === 0 && <p id="explainer">Use the form below to get started!</p>}
-			{dialogues.length > 0 && <header id="topmenu">
+			{blocks.length === 0 && <p id="explainer">Use the form below to get started!</p>}
+			{blocks.length > 0 && <header id="topmenu">
 				<button id="saveimage" onClick={screenshot}>Save as PNG</button>
 				<button id="clearall" onClick={clearAll}>Delete all</button>
 			</header>}
 
-			{dialogues.length > 0 && <div id="dialogues">
-				{dialogues.map((obj, index, array) => <DialogueUnit key={obj.id} {...obj} allDialogues={dialogues} setDialogues={setDialogues} isFirst={index === 0} isLast={index === array.length - 1} />)}
-			</div>}
+			{blocks.length > 0 && <section id="dialogues">
+				{blocks.map((obj, index, array) => {
+					return "options" in obj
+						? <MultipleChoice key={obj.id} {...obj} />
+						: <DialogueBlock key={obj.id} {...obj} allDialogues={blocks} setDialogues={setBlocks} isFirst={index === 0} isLast={index === array.length - 1} />
+				})}
+			</section>}
 
-			<AddForm setDialogues={setDialogues} />
+			<AddForm setDialogues={setBlocks} />
 
-			<JSONInput dialogues={dialogues} setDialogues={setDialogues} />
+			<JSONInput dialogues={blocks} setDialogues={setBlocks} />
 
-			<div id="downloadasimage" hidden>
+			<section id="downloadasimage" hidden>
 				{/* Placeholder URL to avoid a11ty warning */}
 				<a id="imgdownload" href="/misc/someone_shadow.png" download="beebo-dialogue.png">Download as image</a>
 				<canvas id="imgcanvas" />
-			</div>
+			</section>
 		</main>
+
 		<footer id="sitefoot">
 			<p><a href="https://github.com/wovenstarlight">Made by wovenstarlight</a> | <a href="https://github.com/wovenstarlight/beebo-dialogue">Source code on Github</a></p>
 			<p>Font credits: <a href="https://www.dafont.com/nokia-cellphone.font">Nokia Cellphone FC</a> and SqueezedPixels by Bwobbers</p>
