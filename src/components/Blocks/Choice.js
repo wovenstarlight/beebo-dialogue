7c18e885be24573f54f27683952ce3f3eb7315fe
Devishi Ranjan <drconker@gmail.com>
2025-02-17 14:50:25 +0530

    Add IDs to choice blocks

--- src/components/Blocks/Choice.js
+++ src/components/Blocks/Choice.js
@@ -5,8 +5,12 @@ function ChoiceBlock({
 	allBlocks, setBlocks,
 	isFirst, isLast,
 }) {
-	return <article className={`mcq ${color ?? "purple"}`}>
-		{options.map((opt, index) => <p key={`${id}_${index}`} className={opt.selected && "selected"}>{opt.text}</p>)}
+	return <article id={id} className={`mcq ${color ?? "purple"}`}>
+		{options.map((opt, index) => <p
+			key={`${id}_${index}`}
+			id={`${id}_${index}`}
+			className={opt.selected && "selected"}
+		>{opt.text}</p>)}
 	</article>;
 }
 
