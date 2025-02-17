1d3266a12f0ed6f2fa47059f738204386d6577b0
Devishi Ranjan <drconker@gmail.com>
2025-02-17 16:03:18 +0530

    Add whitespace for readibility

--- src/components/Blocks/Choice.js
+++ src/components/Blocks/Choice.js
@@ -1,11 +1,20 @@
 import "../../styles/Choice.css";
 
 export default function ChoiceBlock({ data: { id, color, options } }) {
-	return <article id={id} className={`mcq ${color ?? "purple"}`}>
-		{options.map((opt, index) => <p
-			key={`${id}_${index}`}
-			id={`${id}_${index}`}
-			className={`${opt.color ?? "purple"}${opt.selected ? " selected" : ""}`}
-		>{opt.text}</p>)}
-	</article>;
+	return (
+	<article
+		id={id}
+		className={`mcq ${color ?? "purple"}`}
+	>
+		{options.map((opt, index) => (
+			<p
+				key={`${id}_${index}`}
+				id={`${id}_${index}`}
+				className={`${opt.color ?? "purple"}${opt.selected ? " selected" : ""}`}
+			>
+				{opt.text}
+			</p>
+		))}
+	</article>
+	);
 }
