442d1fadd9321685de284210af23c03953fabf5a
Devishi Ranjan <drconker@gmail.com>
2025-02-18 15:43:02 +0530

    Create resources for i18n

---
+++ src/i18n/en.js
@@ -0,0 +1,20 @@
+const MESSAGES = {
+	SITE_HEADER: {
+		TITLE: "Detective Beebo dialogue builder",
+		SUBTITLE: "Based on the game <hyperlink>Detective Beebo: Night at the Mansion</hyperlink> by Bwobbers",
+		GIF_MAKER: "A static image version of <hyperlink>kongkrog's dialogue GIF maker</hyperlink>",
+	},
+
+	BODY: {
+		STARTUP: "Use the form below to get started!",
+	},
+
+	FOOTER: {
+		CREATOR: "Made by wovenstarlight",
+		SOURCE_CODE: "Source code on Github",
+		FONTS: "Font credits: <hyperlinkNokia>Nokia Cellphone FC</hyperlinkNokia> and SqueezedPixels by Bwobbers",
+	},
+};
+Object.freeze(MESSAGES);
+
+export default MESSAGES;
