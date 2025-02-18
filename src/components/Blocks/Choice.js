b8404c323f16106aa22fc065f4a0c90db29831bf
Devishi Ranjan <drconker@gmail.com>
2025-02-18 19:23:00 +0530

    i18n Dialogue/Choice blocks

--- src/components/Blocks/Choice.js
+++ src/components/Blocks/Choice.js
@@ -1,6 +1,10 @@
+import { useTranslation } from "react-i18next";
+import { DEFAULT_CHOICE } from "../../constants/blockDefaults";
 import "../../styles/Choice.css";
 
 export default function Choice({ id, options }) {
+	const { t } = useTranslation();
+
 	return <>
 		{options.map((opt, index) => (
 			<p
@@ -8,7 +12,7 @@ export default function Choice({ id, options }) {
 				id={`${id}_${index}`}
 				className={opt.selected ? "selected" : undefined}
 			>
-				{opt.text}
+				{opt.text ?? t(DEFAULT_CHOICE.text)}
 			</p>
 		))}
 	</>;
--- src/components/Blocks/Dialogue.js
+++ src/components/Blocks/Dialogue.js
@@ -1,15 +1,18 @@
+import { useTranslation } from "react-i18next";
 import { DEFAULT_DIALOGUE } from "../../constants/blockDefaults";
 import "../../styles/TextBox.css";
 
 export default function Dialogue({ speaker, portrait, dialogue }) {
+	const { t } = useTranslation();
+
 	return <>
 		<header className="speaker">
 			<span className="question" aria-hidden={true}>?</span>
-			<span className="name">{speaker ?? DEFAULT_DIALOGUE.speaker}</span>
+			<span className="name">{speaker ?? t(DEFAULT_DIALOGUE.speaker)}</span>
 		</header>
 		<div className="body">
 			<img className="portrait" alt="" src={`${process.env.PUBLIC_URL}/assets/portraits/`.concat(portrait ?? DEFAULT_DIALOGUE.portrait)} aria-hidden="true" />
-			<p className="text">{dialogue ?? DEFAULT_DIALOGUE.dialogue}</p>
+			<p className="text">{dialogue ?? t(DEFAULT_DIALOGUE.dialogue)}</p>
 		</div>
 	</>;
 }
